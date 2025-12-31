ba7bee04da67dec655d4c7447b0f3a212e77ff30
Sebastian <seb0465@hotmail.com>
2025-12-31 05:26:11 +0100

    fire and forget

--- CqrsDesign/Cqrs.Application/Commands/CreateOrderHandler.cs
+++ CqrsDesign/Cqrs.Application/Commands/CreateOrderHandler.cs
@@ -22,7 +22,22 @@ public class CreateOrderHandler
         _write.Orders.Add(order);
         await _write.SaveChangesAsync();
 
-        await _read.Orders.InsertOneAsync(order);
+        // Fire-and-forget of read db
+        // Vi forholder os simpelt da vi bare vil have noget der ville være representabelt af en cqrs struktur
+        // I et rigtigt system ville vi lave et system med service for read db og service for write db, samt kommunikation imellem.
+        // Derved ville der kunne være koordinering af opdateringer, og skabe en mere robust løsning.
+        _ = Task.Run(async () =>
+        {
+            try
+            {
+                await _read.Orders.InsertOneAsync(order);
+            }
+            catch (Exception ex)
+            {
+                // Log fejl, retry hvis nødvendigt
+                Console.WriteLine($"Fejl ved opdatering af read DB: {ex.Message}");
+            }
+        });
 
         return order.Id;
     }
